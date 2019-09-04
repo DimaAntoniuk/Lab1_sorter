930d264e1a8795ce2f106b39c88f32636a09edac
Dima Antoniuk <dimaantoniuk09@gmail.com>
2019-09-04 11:58:40 +0300

    added swaps counter and compares counter

--- Main.py
+++ Main.py
@@ -1,9 +1,11 @@
 from models.Vase import Vase
+import time
 
 def merge(A, B):
     a = b = 0
     out = []
     while a < len(A) and b < len(B):
+        merge_compare += 1
         if A[a].weight < B[b].weight:
             out.append(A[a])
             a += 1
@@ -23,9 +25,11 @@ def selectionSort(A):
     for i in range(len(A)):
         min_i = i
         for j in range(i+1, len(A)):
+            selection_compare += 1
             if A[j].volume > A[min_i].volume:
                 min_i = j
         A[i], A[min_i] = A[min_i], A[i]
+        selection_swap += 1
 
 class File(object):
     def __init__(self, file_name, method):
@@ -33,8 +37,8 @@ class File(object):
     def __enter__(self):
         return self.file_obj
     def __exit__(self, type, value, traceback):
-        self.file_obj.close()
 
+        self.file_obj.close()
 arr = []
 
 with File('input.txt', 'r') as input:
@@ -51,20 +55,34 @@ with File('input.txt', 'r') as input:
         if(i % 3 == 0):
             arr.append(Vase(volume, weight, field))
 
+selection_start = time.time()
+selection_compare = 0
+selection_swap = 0
 selectionSort(arr)
+selection_finish = time.time()
 
 with File('output.txt', 'a+') as output:
     output.write('SELECTION SORT\n')
+    output.write('Time:' + str(selection_finish-selection_begin) + '\n')
+    output.write('Compares:' + str(selection_compare) + '\n')
+    output.write('Swaps:' + str(selection_swap) + '\n')
     output.write('Elements:\n')
     for a in arr:
         output.write(str(a))
         if a.material.find('\n') == -1:
             output.write('\n')
 
+merge_begin = time.time()
+merge_compare = 0
+merge_swap = 0
 arr = mergeSort(arr)
+merge_finish = time.time()
 
 with File('output.txt', 'a+') as output:
     output.write('MERGE SORT\n')
+    output.write('Time:' + str(merge_finish-merge_begin) + '\n')
+    output.write('Compares' + str(merge_compare) + '\n')
+    output.write('Swaps' + srt(merge_swap) + '\n')
     output.write('Elements:\n')
     for a in arr:
         output.write(str(a))
